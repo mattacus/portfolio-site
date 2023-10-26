fe42fdfab15a8994aa7bd1a06bd7bd73efc0a730
mattl <matt.lee.personal@gmail.com>
2023-10-26 00:34:21 -0500

    profile page updates

--- src/ProfileCard.tsx
+++ src/ProfileCard.tsx
@@ -1,9 +1,9 @@
-import "./profileCard.css";
-import profile from "./data/profile.json";
+import './profileCard.css';
+import profile from './data/profile.json';
 
 const ProfileCard: React.FunctionComponent = () => {
   const { name, title, message_link, connect_link, skills } = profile;
-  const profileImg = new URL("./img/profile/profile.jpg", import.meta.url);
+  const profileImg = new URL('./img/profile/profile.jpg', import.meta.url);
   return (
     <div className="card-container">
       <img
@@ -14,10 +14,26 @@ const ProfileCard: React.FunctionComponent = () => {
         alt="profile-photo"
       />
       <h3>{name}</h3>
-      <p>{title}</p>
+      <h4>{title}</h4>
       <div className="buttons">
-        <button className="primary">Message</button>
-        <button className="primary ghost">Connect</button>
+        <button
+          className="primary"
+          onClick={() => {
+            let a = document.createElement('a');
+            a.href = message_link;
+            a.click();
+          }}
+        >
+          Message
+        </button>
+        <button
+          className="primary"
+          onClick={() => {
+            window.open(connect_link);
+          }}
+        >
+          Connect
+        </button>
       </div>
       <div className="skills">
         <h6>Skills</h6>
