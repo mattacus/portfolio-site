1a16d960503a9bd3025854122d5fc69791bbed43
mattl <matt.lee.personal@gmail.com>
2024-01-22 16:59:04 -0600

    add github link

--- src/ProfileCard.tsx
+++ src/ProfileCard.tsx
@@ -2,10 +2,11 @@ import "./profileCard.css";
 import profile from "./data/profile.json";
 
 const ProfileCard: React.FunctionComponent = () => {
-  const { name, title, message_link, connect_link, bio, skills } = profile;
+  const { name, title, message_link, connect_link, github_link, bio, skills } = profile;
   const profileImg = new URL("./img/profile/profile.jpg", import.meta.url);
   const messageIcon = new URL("./img/icons/email-48.png", import.meta.url);
   const connectIcon = new URL("./img/icons/linkedin-48.png", import.meta.url);
+  const githubIcon = new URL("./img/icons/gh-48.png", import.meta.url);
 
   return (
     <div className="card-container border-gradient-profile">
@@ -43,7 +44,19 @@ const ProfileCard: React.FunctionComponent = () => {
             <img src={String(connectIcon)} alt="icon" width={24} height={24} />
           </div>
         </button>
+        <button
+          className="primary"
+          onClick={() => {
+            window.open(github_link);
+          }}
+        >
+          <div className="button-inner">
+            Github
+            <img src={String(githubIcon)} alt="icon" width={24} height={24} />
+          </div>
+        </button>
       </div>
+
       <hr />
       {bio.map((b) => (
         <p className="bio" key={b}>
